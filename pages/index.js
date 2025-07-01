b416b647e3de5d79a2f73b737bc96a46f507a2f3
Recicladoraeth <recicladoraeth@gmail.com>
2025-07-01 12:58:16 -0300

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ fontFamily: 'Arial', textAlign: 'center', marginTop: '100px' }}>
-      <h1>Welcome to the HeadCoin Presale</h1>
-      <p>🌱 Crypto for a cleaner world.</p>
-    </div>
-  );
-}
